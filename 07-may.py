817485b93709801b8fe099c59fe85ec292ef36e1
sudhirkushwaha10-hue <sudhirkushwaha10@gmail.com>
2026-05-09 11:34:53 +0530

    update

---
+++ 07-may.py
@@ -0,0 +1,108 @@
+# nasted if changing/ladder statement
+# name="aman"
+# if name:
+#     print('yes name dev is provider')
+# else:
+#     print("Name is not provider") 
+
+# name="dev"
+# if name == "dev":
+#     print('yes name dev is provider')
+# else:
+#     print("Name is not provider")
+
+# name="dev"
+# if name == "":
+#     print('yes name dev is provider')
+# else:
+#     print("Name is not provider")
+
+# name="dev"
+# if name == "":
+#     print('yes name dev is provider')
+# else:
+#     print("Name is not provider")
+
+#     name="dev"
+# if name:
+#     print(f'yes {name} is provider')  #{} bracket is used to address the details which is inside the bracket
+# else:
+#     print("Name is not provider")
+
+# name="dev"
+# if name:
+#     print(f'yes {name} is provider') 
+#     adress="noida"
+#     # if address:
+    #     print(f"yes aderess is {address} provided")
+    # else:
+    #     print(f'no address not provided')
+# else:
+#     print("Name is not provider")
+
+
+
+
+# n1=20
+# if n1%2==0:
+#     print('even')
+#     mob=input("enter your number:")
+#     if len(mob)==10:
+#         print("valid number")
+#     else:
+#         print("invalid number")
+
+# pre=input("enter your pre marks:")
+# if pre >= "400":
+#     print("your are pass in pre and eligible in mains")
+# else:
+#     print("better next time, your failed ❌")
+
+
+  
+# pre=input("enter your pre marks:")
+# if pre >= "400":
+#     print("your are pass in pre and eligible in mains")
+#     mains=input("enter your main marks")
+#     if mains>= "600":
+#        print("your are pass in mains")
+# else:
+#     print("better next time, your failed ❌")
+    
+
+# pre=int(input("enter your pre marks:"))
+# if pre >= 400:
+#     print("your are pass in pre and eligible in mains")
+#     mains=int(input("enter your main marks"))
+#     if mains>= 600:
+#        print("your are pass in mains, selected for interview")
+#     else:
+#         print("your r fail in mains")
+#     interview=int(input("enter your interview number"))
+#     if interview>=300:
+#         print("congratulation, your r selected for UPSE Rajya sangSeva")
+#     else:
+#         print("we r apolozied to say u r just to missed ❌")
+    
+# else:
+#     print("better next time, your failed ❌")
+
+
+pre=int(input("enter your pre marks:"))
+if pre >= 400:
+    print("your are pass in pre and eligible in mains")
+    mains=int(input("enter your main marks"))
+    if mains>= 600:
+       print("your are pass in mains, selected for interview")
+
+    interview=int(input("enter your interview number"))
+    if interview>=300:
+        print("congratulation, your r selected for UPSE Rajya SangSeva")
+    
+    
+else:
+    print("better next time, your failed ❌")
+
+  
+  
+
